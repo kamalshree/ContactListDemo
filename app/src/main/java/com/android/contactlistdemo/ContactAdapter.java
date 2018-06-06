d976e991bbf4f59ee736939631eea70c8cb303a9
codesqills <codesqills@gmail.com>
2018-06-05 22:20:24 -0400

    Contact App Implementation with color change Circles

--- app/src/main/java/com/android/contactlistdemo/ContactAdapter.java
+++ app/src/main/java/com/android/contactlistdemo/ContactAdapter.java
@@ -1,20 +1,21 @@
 package com.android.contactlistdemo;
 
 import android.content.Context;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,13 +26,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
 
     private static final String TAG = "ContactAdapter";
-    ArrayList<String> ContactImage=new ArrayList<>();
+
     ArrayList<String> ContactName=new ArrayList<>();
     ArrayList<String> ContactNumber=new ArrayList<>();
     Context mContext;
 
-    public ContactAdapter( Context mContext,ArrayList<String> contactImage, ArrayList<String> contactName,ArrayList<String> contactNumber) {
-        ContactImage = contactImage;
+    public ContactAdapter( Context mContext, ArrayList<String> contactName,ArrayList<String> contactNumber) {
         ContactName = contactName;
         ContactNumber = contactNumber;
         this.mContext = mContext;
@@ -48,14 +48,19 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: OnBindViewHolder");
+        String fChar = String.valueOf(ContactName.get(position).charAt(0));
 
-        Glide.with(mContext)
-                .asBitmap()
-                .load(ContactImage.get(position))
-                .into(holder.contactImage);
+        Drawable background = holder.contactImage.getBackground();
+
+        if (background instanceof GradientDrawable) {
+            // cast to 'GradientDrawable'
+            GradientDrawable gradientDrawable = (GradientDrawable) background;
+            gradientDrawable.setColor(ContextCompat.getColor(mContext, getRandomColor()));
+        }
 
         holder.contactName.setText(ContactName.get(position));
         holder.contactNumber.setText(ContactNumber.get(position));
+        holder.contactInitial.setText(fChar);
 
         holder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,25 +70,77 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         });
     }
 
+    //random color generator
+    private int getRandomColor() {
+            Random r = new Random();
+            int randNumber = r.nextInt(10 - 1) + 1;
+            int color_val=0;
+            switch (randNumber){
+
+                case 1:{
+                    color_val= R.color.color1;
+                    break;
+                }
+                case 2:{
+                    color_val= R.color.color2;
+                    break;
+                }
+                case 3:{
+                    color_val= R.color.color3;
+                    break;
+                }
+                case 4:{
+                    color_val= R.color.color4;
+                    break;
+                }
+                case 5:{
+                    color_val= R.color.color5;
+                    break;
+                }
+                case 6:{
+                    color_val= R.color.color6;
+                    break;
+                }
+                case 7:{
+                    color_val= R.color.color7;
+                    break;
+                }
+                case 8:{
+                    color_val= R.color.color8;
+                    break;
+                }
+                case 9:{
+                    color_val= R.color.color9;
+                    break;
+                }
+                case 10:{
+                    color_val= R.color.color10;
+                    break;
+                }
+
+            }
+        return color_val;
+    }
+
     @Override
     public int getItemCount() {
-        return ContactImage.size();
+        return ContactName.size();
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         CircleImageView contactImage;
-        TextView contactName,contactNumber;
+        TextView contactName,contactNumber,contactInitial;
         ConstraintLayout parentLayout;
         public ViewHolder(View itemView) {
             super(itemView);
 
             parentLayout=itemView.findViewById(R.id.rl_constraintlayout);
             contactImage=itemView.findViewById(R.id.im_image);
+            contactInitial=itemView.findViewById(R.id.tv_initial);
             contactName=itemView.findViewById(R.id.tv_name);
             contactNumber=itemView.findViewById(R.id.tv_contact);
         }
     }
-
 }
